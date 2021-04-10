e255c45e08e53fad7aa9f163dd7092e9f8744155
Alex Petrenko <petrenkodesin@gmail.com>
2021-04-10 22:40:12 +0300

    popup for images+allcarusels

--- js/main.js
+++ js/main.js
@@ -1,26 +1,91 @@
 $( document ).ready(function() { // site load
 
-  // Moments OWL carusel starting
+  // Moments and Lessons OWL carusel starting
   $('.video-owl-carousel').owlCarousel({
-    loop:true,
+    loop:false,
     nav:false,
     dots:false,
     margin:10,
+    mouseDrag:false,
+    responsiveClass:true,
+    responsive:{
+      0:{
+        stagePadding: 50,
+        items:1
+      },
+      600:{
+        stagePadding: 50,
+        items:2
+      },
+      1000:{
+        items:3,
+        nav:true
+      }
+    }
+  });
+
+  // Material OWL carusel starting
+  $('.material-owl-carousel').owlCarousel({
+    loop:false,
+    nav:false,
+    dots:false,
+    mouseDrag:false,
     responsiveClass:true,
     responsive:{
       0:{
         stagePadding: 50,
         items:1,
-        loop:false
+      },
+      600:{
+        stagePadding: 50,
+        items:3
+      },
+      1000:{
+        items:4
+      }
+    }
+  });
+
+  // Gallery OWL carusel starting
+  $('.image-owl-carousel').owlCarousel({
+    loop:false,
+    nav:false,
+    dots:false,
+    margin:10,
+    mouseDrag:false,
+    responsiveClass:true,
+    responsive:{
+      0:{
+        stagePadding: 20,
+        items:1,
       },
       600:{
         items:3
       },
       1000:{
-        items:3,
-        nav:true,
-        loop:false,
-        mouseDrag:false
+        items:3
+      }
+    }
+  });
+
+  // Reviews OWL carusel starting
+  $('.review-owl-carousel').owlCarousel({
+    loop:false,
+    nav:false,
+    dots:false,
+    margin:10,
+    mouseDrag:false,
+    responsiveClass:true,
+    responsive:{
+      0:{
+        stagePadding: 50,
+        items:1,
+      },
+      600:{
+        items:2
+      },
+      1000:{
+        items:2
       }
     }
   });
@@ -102,3 +167,74 @@ function openFullscreen(elem) {
     elem.msRequestFullscreen();
   }
 }
+
+// Popup window for gallery
+$('.gallery .item').on('click', function() {
+  var carusel='';
+  var title = $(this).find('img').attr('alt');
+  var current_item = $(this).attr('data-id');
+  var total_items = $(this).length;
+
+  $('.gallery .item').each(function(i){
+    var card_img = $(this).find('img').attr('src');
+    var card_title = $(this).find('img').attr('alt');
+    var card_id = $(this).attr('data-id');
+
+    carusel += '<div data-id="' + card_id  + '" class="carousel-item';
+    if(current_item===card_id) carusel += ' active';
+    carusel += '">';
+    carusel += '<img src="' + card_img + '" alt="' + card_title + '" class="d-block w-100">';
+    carusel += '</div>';
+  });
+
+  $('#matPop .carousel-inner').html(carusel);
+  $('#matPop .title').html(title);
+  $('#matPop .items-num .current-item').text(current_item);
+  $('#matPop .items-num .total-item').text(total_items);
+  $('#matPop').modal('show');
+});
+
+// Popup window for material content
+$('.materials .card').on('click', function() {
+  var carusel='';
+  var title = $(this).find('.card-title').html();
+  var current_item = $(this).attr('data-id');
+  var total_items = $('.materials .card').length;
+
+  $('.materials .card').each(function(i){
+    var card_img = $(this).find('img').attr('src');
+    var card_title = $(this).find('.card-title').html();
+    var card_id = $(this).attr('data-id');
+
+    carusel += '<div data-id="' + card_id  + '" class="carousel-item';
+    if(current_item===card_id) carusel += ' active';
+    carusel += '">';
+    carusel += '<img src="' + card_img + '" alt="' + card_title + '">';
+    carusel += '</div>';
+  });
+
+  $('#matPop .carousel-inner').html(carusel);
+  $('#matPop .title').html(title);
+  $('#matPop .items-num .current-item').text(current_item);
+  $('#matPop .items-num .total-item').text(total_items);
+  $('#matPop').modal('show');
+});
+
+// Material modal Slider numerology
+$('#matPop').on('slid.bs.carousel', function() {
+  var total_items = $(this).find('.carousel-item').length;
+  var curent_item = parseInt($(this).find('.carousel-inner .active').attr('data-id'));
+  var title = $(this).find('.carousel-inner .active img').attr('alt');
+
+  $(this).find('.items-num .current-item').text(curent_item);
+  $(this).find('.items-num .total-item').text(total_items);
+  $(this).find('.title').html(title);
+})
+
+// Reset material modal data
+$('#matPop').on('hidden.bs.modal', function (e) {
+  $('#matPop .carousel-inner').html('<div class="carousel-item active"><img src="img/image.jpg"></div>');
+  $('#matPop .items-num .current-item').text(0);
+  $('#matPop .items-num .total-item').text(0);
+  $('#matPop .title').html("Title");
+})
